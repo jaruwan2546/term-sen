2ebba881df746d86e282447a842be46ea3f40dd9
jaruwan2546 <jaruwan2546.h20p@gmail.com>
2026-08-01 19:37:16 +0700

    Create History.jsx

---
+++ src/pages/History.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+
+function History() {
+
+  const [sales, setSales] = useState([]);
+
+  const loadSales = async () => {
+
+    const data = await getDocs(
+      collection(db, "sales")
+    );
+
+    setSales(
+      data.docs.map((item) => ({
+        ...item.data(),
+        id: item.id
+      }))
+    );
+
+  };
+
+
+  useEffect(() => {
+    loadSales();
+  }, []);
+
+
+  return (
+    <div className="app">
+
+      <h1>🧾 ประวัติการขาย</h1>
+
+
+      {sales.map((item) => (
+
+        <div key={item.id}>
+
+          <h3>
+            🛒 {item.name}
+          </h3>
+
+          <p>
+            📦 จำนวน: {item.qty} ชิ้น
+          </p>
+
+          <p>
+            💰 ยอดขาย: {item.total} บาท
+          </p>
+
+          <p>
+            📈 กำไร: {item.profit || 0} บาท
+          </p>
+
+          <p>
+            🕒 {item.date?.toDate
+              ? item.date.toDate().toLocaleString()
+              : ""
+            }
+          </p>
+
+          <hr />
+
+        </div>
+
+      ))}
+
+
+    </div>
+  );
+}
+
+export default History;
