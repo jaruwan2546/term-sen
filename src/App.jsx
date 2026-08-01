27be78ed2cb238328d386b7bf0b1b1404a36d85e
jaruwan2546 <jaruwan2546.h20p@gmail.com>
2026-08-01 13:57:05 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>TERM SEN</h1>
+      <p>ระบบจัดการร้านค้าออนไลน์</p>
+
+      <div className="menu">
+        <button>🏠 Dashboard</button>
+        <button>📦 สินค้า</button>
+        <button>⚙️ ตั้งค่า</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
