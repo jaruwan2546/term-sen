9b222e5bede608790cc87e61a2516838f0c8203a
jaruwan2546 <jaruwan2546.h20p@gmail.com>
2026-08-01 15:09:35 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // Import the functions you need from the SDKs you need
+  / Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,5 +22,7 @@ const app = initializeApp(firebaseConfig);
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
 
 export default app;
