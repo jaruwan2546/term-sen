1b3f124a7c42576a6ccc976040627ea98af24f23
jaruwan2546 <jaruwan2546.h20p@gmail.com>
2026-08-03 08:50:38 +0700

    Create StockIn.jsx

---
+++ src/pages/StockIn.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function StockIn() {
+  return (
+    <div className="app">
+      <h1>📥 รับสินค้าเข้า</h1>
+
+      <p>หน้านี้กำลังพัฒนา...</p>
+    </div>
+  );
+}
+
+export default StockIn;
