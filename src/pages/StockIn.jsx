e6255cf947917ea9d08d9e540549013c243e9eba
jaruwan2546 <jaruwan2546.h20p@gmail.com>
2026-08-03 08:58:56 +0700

    Update StockIn.jsx

--- src/pages/StockIn.jsx
+++ src/pages/StockIn.jsx
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 function StockIn() {
+
+  const [products, setProducts] = useState([]);
+  const [selected, setSelected] = useState("");
+
+  const loadProducts = async () => {
+
+    const data = await getDocs(
+      collection(db, "products")
+    );
+
+    setProducts(
+      data.docs.map((item) => ({
+        ...item.data(),
+        id: item.id
+      }))
+    );
+  };
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
   return (
     <div className="app">
+
       <h1>📥 รับสินค้าเข้า</h1>
 
-      <p>หน้านี้กำลังพัฒนา...</p>
+      <select
+        value={selected}
+        onChange={(e) => setSelected(e.target.value)}
+      >
+
+        <option value="">
+          เลือกสินค้า
+        </option>
+
+        {products.map((item) => (
+          <option
+            key={item.id}
+            value={item.id}
+          >
+            {item.name}
+          </option>
+        ))}
+
+      </select>
+
     </div>
   );
 }
